aae764b6981dc118455eea6a3617c6a7cc520283
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2022-11-26 21:14:03 +0900

    Setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React, { Component } from 'react';
+import Main from './Main';
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './NotFound';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
