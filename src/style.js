088ae99dd2756ce2508b9e9d475f159cb0b299c4
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2022-12-01 17:55:28 +0900

    Update vh

--- src/style.js
+++ src/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Body = styled.div`
     display: flex;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     justify-content: center;
     align-items: center;
 `;
@@ -11,7 +11,7 @@ export const View = styled.div`
     display: flex;
     justify-content: center;
     width: 720px;
-    height: 100%;
+    height: 100vh;
     background-color: #f2f3f7;
     box-shadow: 0px 0px 23px -15px rgb(0, 0, 0);
 `;
