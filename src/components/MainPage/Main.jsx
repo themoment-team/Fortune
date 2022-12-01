b84cceef222875ae6e32d30bb4e39f0944d68dd1
yebin0310 <yebin060310@gmail.com>
2022-12-01 19:34:43 +0900

    Update main

--- src/components/MainPage/Main.jsx
+++ src/components/MainPage/Main.jsx
@@ -4,8 +4,12 @@ const Main = () => {
 	return (
 		<div>
 			<div className="container">
-				<div className="title">
-					<S.text>하이</S.text>
+				<div className="container-title">
+					<S.text>Fortune</S.text>
+				</div>
+				<div className="container-buttons">
+					<S.button>운세보기</S.button>
+					<S.button>궁합보기</S.button>
 				</div>
 			</div>
 		</div>
--- src/components/MainPage/style.js
+++ src/components/MainPage/style.js
@@ -3,3 +3,6 @@ import styled from 'styled-components';
 export const text = styled.h1`
     font-size: 500%;
 `;
+
+export const button = styled.button`
+`;
