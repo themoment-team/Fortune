49031830298b62a9d95c5cf435564a1ed1c99b5d
handsomejeongwoo <comand123@naver.com>
2022-12-16 18:04:36 +0900

    Add ResultFortunePage at index

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,6 @@
-export { default as Fortune } from './FortunePage';
-export { default as Main } from './MainPage';
-export { default as Match } from './MatchPage';
-export { default as MatchResult } from './MatchResult';
-export { default as NotFound } from './NotFoundPage';
+export { default as Fortune } from "./FortunePage";
+export { default as Main } from "./MainPage";
+export { default as Match } from "./MatchPage";
+export { default as MatchResult } from "./MatchResult";
+export { default as NotFound } from "./NotFoundPage";
+export { default as ResultFortunePage } from "./ResultFortunePage";
